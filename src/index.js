b851d11282e2fded932335b0dff6fea20fb41dfe
Jonathan <sunyang713@hotmail.com>
2017-01-27 13:32:34 -0500

    Refactor.
     - Specify either a top-level app substate or supply a selector
       function.
     - Supply actions as an object of actions instead of array
     - Remove `component` and `optionalMethods` parameters

--- src/index.js
+++ src/index.js
@@ -1,77 +1,49 @@
-// ==========================
-// Reduxify JS
-// desc: Development tools to automatically bind actions and storestate to props.
-// Author: Brian Boyko <brian.boyko@gmail.com>
-// For compatability reasons, this is written in ES5.
-// ==========================
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-var bindActionCreators = require('redux').bindActionCreators;
-var connect = require('react-redux').connect;
+/**
+ * Enhances the <react-redux/connect> function.
+ *
+ * @param state <String>              The name of the desired state.
+ *                                    The component will be given read-access to this state.
+ * @param selector <function>         selector mapping app states to component props.
+ *                                    A memoized selector function describing selection logic.
+ *                                    The selector efficiently computes derived data from the store.
+ * @param actions <Object>[function]  Object of actions.
+ *                                    The component will be allowed to execute these actions.
+ * @return <function>                 an enhanced <connect> function.
+ */
+export default function reduxify({ state, selector, actions }) {
+  var mapStateToProps
+  var mapDispatchToProps
 
-// this standAlonePick is a reimplementation of "pick" from libraries such as
-// lodash and underscore. Since we only need this one function, it is perhaps
-// best to simply use it here.
+  if typeof state !== 'string'
+    throw new Error('State parameter must be a string.')
 
-function standAlonePick(state, list) {
-  return list.reduce(function(prev, curr) {
-    prev[curr] = state[curr];
-    return prev;
-  }, {});
+  if typeof selector !== 'function'
+    throw new Error('Selector must be a function.')
+
+  if typeof actions !== 'object'
+    throw new Error('Actions must be an object of functions.')
+
+  // the component will subscribe to Redux store updates
+  if (state)
+    mapStateToProps = STATE => ({
+      [state]: STATE[state]
+    })
+
+  // the component will subscribe to Redux store updates per a selector
+  if (selector)
+    mapStateToProps = selector
+
+  // the component will be provided actions
+  if (actions)
+    mapDispatchToProps = dispatch => ({
+      actions: bindActionCreators(actions, dispatch)
+    })
+
+  return connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
 }
-
-/* ************************************
-Parameters:
-  actions         | @object[containing objects]: An object, which contains all
-                    the action creators of the application. Since actioncreators
-                    are constants, there should be no performance detriment for
-                    including them all.
-  keyList         | @array[containing strings]: An array of strings, listing all
-                    the keys in the redux store that the user wishes to bind to
-                    this.props for this component.
-                    Only the keys necessary for the component's function should
-                    be included, so that the component does not re-render when
-                    the function changes.
-  component       | @React.Component: The react component to which to bind the
-                    props.
-  optionalMethods | @object(optional)[containing methods]: if the user wishes to
-                    bind other methods to the component's props, the user can
-                    specify these methods here. One scenario which this might be
-                    useful is if the user wants to pass in getState() as a prop.
-************************************ */
-
-module.exports = function reduxify(actions, keyList, component, optionalMethods) {
-
-  var mapStateToProps = function(state) {
-    return standAlonePick(state, keyList);
-  };
-
-  var prepareActions = function(actions) {
-    return function(dispatch) {
-      // this is incase the user passes in the action creatores as "actions.default"
-      // instead of "actions";
-      if (actions.hasOwnProperty('default') && Object.keys(actions).length ===
-        1) {
-        actions = actions.default;
-      }
-
-      var output = {
-        actions: bindActionCreators(actions, dispatch),
-        dispatch: dispatch,
-      };
-      // Here, we add any optional methods the user has supplied.
-      // optionalMethods does not need to be supplied.
-      if (optionalMethods && typeof(optionalMethods) === "object") {
-        for (var key in optionalMethods) {
-          output[key] = optionalMethods[key];
-        }
-      }
-      return output;
-    };
-  };
-
-  var mapDispatchToProps = function(dispatch) {
-    return prepareActions(actions, dispatch);
-  };
-
-  return connect(mapStateToProps, mapDispatchToProps)(component);
-};
